23a6c9864393bebc934c36fb6a35d760d1af19e9
Your Name <your_email@address>
2024-02-08 12:05:41 +0000

    font updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import "../styles/global.css";
 import { ThemeProvider } from "next-themes";
+import { Inconsolata } from "next/font/google";
+
+const inconsolata = Inconsolata({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
-      <Component {...pageProps} />
+      <main className={inconsolata.className}>
+        <Component {...pageProps} />
+      </main>
     </ThemeProvider>
   );
 }
