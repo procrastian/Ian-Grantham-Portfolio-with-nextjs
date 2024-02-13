be29c8207e1798ace4e45bc2952a7b92741bf294
Your Name <your_email@address>
2024-02-13 17:02:13 +0000

    github and linked icons added with animation

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import utilStyles from "../styles/utils.module.css";
 import { getSortedProjectsData } from "../lib/projects";
 import Link from "next/link";
 import Date from "../components/date";
+import { AiFillGithub } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiFillMail } from "react-icons/ai";
 
 export async function getStaticProps() {
   const allProjectsData = getSortedProjectsData();
@@ -21,8 +24,19 @@ export default function Home({ allProjectsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
+        <Link href={"https://github.com/procrastian"} target="_blank">
+          <AiFillGithub className="icon"/>
+        </Link>
+        <Link href={"http://www.linkedin.com/in/dev-ian-grantham"} target="_blank">
+          <AiFillLinkedin className="icon"/>
+        </Link>
+        {/* <Link href={"mailto:dev.iangrantham@gmail.com"} target="_blank"><AiFillMail /></Link> */}
+      </section>
+      <section className={utilStyles.headingMd}>
         <p>
-          My name is Ian Grantham, I'm a junior full stack software engineer based in the UK. I'm passionate about problem solving modern web development problems, while creating engaging UX focussed designs.
+          My name is Ian Grantham, I'm a junior full stack software engineer
+          based in the UK. I'm passionate about solving modern web development
+          problems, while creating engaging UX focussed designs.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
